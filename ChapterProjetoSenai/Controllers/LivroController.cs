36b8debbaa5c514b7824edef1ca15a56c4cbe00d
João <joao.manfron09@gmail.com>
2022-12-29 13:38:41 -0300

    finalizado

---
+++ ChapterProjetoSenai/Controllers/LivroController.cs
@@ -0,0 +1,97 @@
+﻿using ChapterProjetoSenai.Models;
+using ChapterProjetoSenai.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChapterProjetoSenai.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+        private readonly LivroRepository _livroRepository;
+
+        public LivroController(LivroRepository livroRepository)
+        {
+            _livroRepository = livroRepository;
+        }
+
+
+        [HttpGet]
+        public IActionResult ler()
+        {
+            try
+            {
+                return Ok(_livroRepository.Listar());
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                Livro LivroBuscado = _livroRepository.BuscarPorId(id);
+
+                if (LivroBuscado == null)
+                {
+                    return NotFound("Não encontrado");
+                }
+
+                return Ok(LivroBuscado);
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Livro l)
+        {
+            try
+            {
+                _livroRepository.Cadastro(l);
+                return StatusCode(201);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _livroRepository.Deletar(id);
+                return Ok("Livro Removido com sucesso");
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Alterar(int id, Livro l)
+        {
+            try
+            {
+                _livroRepository.Alterar(id, l);
+                return StatusCode(204);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
